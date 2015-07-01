12f9e87bbba95e4cf478c1fdaa4a9c75345bbe6a
Mikey <michael.jj.allen@gmail.com>
2015-07-01 14:22:44 +0100

    Admin screen basic setup

--- admin.php
+++ admin.php
@@ -1,5 +1,15 @@
-<?php
+<html>
 
-include('php/dbconnection.php');
+<head>
 
-?>
+</head>
+
+<body>
+
+<a href="#">New Post</a>
+<a href="#">Edit Post</a>
+<a href="#">Delete Post</a>
+
+</body>
+
+</html>
